f1f752d1493324855fdc00c9322885dd6769b962
Marcelo <marcelo.leite@mercadolivre.com>
2022-04-12 16:03:04 -0300

    [feat] configuracao inicial do projeto

---
+++ src/test/java/com/bootcampIt/blogs/BlogsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcampIt.blogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
